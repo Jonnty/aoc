d2c43484abbe131654ad74a2e7844dc0bf912f90
jonnty <jonnty@gmail.com>
2018-12-11 23:52:31 +0000

    11 part 2 done, veeery sloowly (but it is NP complete at least I think)

--- 11/11_2.py
+++ 11/11_2.py
@@ -9,13 +9,30 @@ def power_level(x, y):
 	power_level -= 5
 	return power_level
 
+square_sums = {}
 
-levels = []
-for x in xrange(1, 299):
-	for y in xrange(1, 299):
-		levels.append(((x, y), sum((power_level(i,j) for i in [x, x+1, x+2] for j in [y, y+1, y+2]))))
+def square_sum(x, y, size):
 
-print max(levels, key=lambda l: l[1])[0]
+    if not square_sums.has_key((x, y, size)):
+        if size == 1:
+            square_sums[(x, y, size)] = power_level(x, y)
+        else:
+            square_sums[(x, y, size)] = \
+            sum (power_level(i,y) for i in xrange(x, x + size)) + \
+            sum (power_level(x,j) for j in xrange(y, y + size)) + \
+            square_sum(x+1, y+1, size-1)
+    return square_sums[(x, y, size)]
+
+
+def squares():
+	for x in xrange(300, 0, -1):
+		print x
+		for y in xrange(300, 0, -1):
+			for size in xrange(1, min(301-x, 301-y)):
+				params = (x, y, size)
+				yield params, square_sum(*params)
+
+print max(squares(), key=lambda l: l[1])[0]
 
 
 
