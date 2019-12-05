946b8236848e5c87274be371ae1e18d70e1bc1b3
Jonathan Hogg <jonnty@gmail.com>
2019-12-05 20:13:11 +0000

    Much cleaner day5 with stdin/out testing. Hope intcode comes up again!

--- 2019/05/both.py
+++ 2019/05/both.py
@@ -1,26 +1,62 @@
-import os, sys
+import os, sys, inspect
 
-def part1(lines):
+class ExitException(Exception):
+    pass
+
+def both(lines):
     state = [int(e) for e in lines[0].split(',')]
-    #before running the program, replace position 1 with the value 12 and
-    #replace position 2 with the value 2
-
     run_program(state[:])
 
 def read_instruction(instruction):
     instruction = str(instruction)
+    # final two digits are opcode
     opcode = int(instruction[-2:])
+    # first digits are parameter modes in reverse (without leading zeros)
     param_modes = [int(e) for e in reversed(list(instruction[:-2]))]
-
     return (opcode, param_modes)
+
+def count_params(f):
+    return len(inspect.signature(f).parameters)
+
+# output handlers take the result, current state and ip after the params
+# mutate the state if necessary and then return the new ip
+
+def write(result, state, ip):
+    state[state[ip]] = result
+    # skip past the result pointer
+    return ip + 1
+
+def void(result, state, ip):
+    # do nothing
+    return ip
+
+def jump(result, state, ip):
+    # the result of jump functions is the ip to jump to
+    return result if result is not None else ip
+
+def exit():
+    raise ExitException
+
+# opcode: operation, output
+operations = {
+    1: (lambda a, b: a + b, write),
+    2: (lambda a, b: a * b, write),
+    3: (lambda: int(sys.stdin.read(1)), write),
+    4: (lambda a: print(str(a)), void),
+    5: (lambda a, b: b if a != 0 else None, jump),
+    6: (lambda a, b: b if a == 0 else None, jump),
+    7: (lambda a, b: int(a < b), write),
+    8: (lambda a, b: int(a == b), write),
+    99: (exit, void)
+}
 
 def run_program(state):
     ip = 0
-
-    def read_param(n, param_modes):
+    def param(n, modes):
+        # get a parameter from state according to the parameter mode
         mode = 0
-        if len(param_modes) >= n:
-            mode = param_modes[n-1]
+        if len(modes) >= n:
+            mode = modes[n-1]
         if mode == 0:
             # use value as pointer
             return state[state[ip + n]]
@@ -31,44 +67,19 @@ def run_program(state):
             raise Exception("unknown mode %d" % mode)
 
     while True:
-        opcode, param_modes = read_instruction(state[ip])
-        if opcode == 1: #add
-            state[state[ip + 3]] = read_param(1, param_modes) + read_param(2, param_modes)
-            ip += 4
-        elif opcode == 2: #multiply
-            state[state[ip + 3]] = read_param(1, param_modes) * read_param(2, param_modes)
-            ip += 4
-        elif opcode == 3: #input
-            state[state[ip + 1]] = int(sys.stdin.read(1)) #needs newline, yuck
-            ip += 2
-        elif opcode == 4: #output
-            print(str(read_param(1, param_modes)))
-            ip += 2
-        elif opcode == 5: #jump-if-true
-            if read_param(1, param_modes) != 0:
-                ip = read_param(2, param_modes)
-            else:
-                ip += 3
-        elif opcode == 6: #jump-if-false
-            if read_param(1, param_modes) == 0:
-                ip = read_param(2, param_modes)
-            else:
-                ip += 3
-        elif opcode == 7: #less than
-            result = int(read_param(1, param_modes) < read_param(2, param_modes))
-            state[state[ip + 3]] = result
-            ip += 4
-        elif opcode == 8: #equals
-            result = int(read_param(1, param_modes) == read_param(2, param_modes))
-            state[state[ip + 3]] = result
-            ip += 4
-        elif opcode == 99: #halt
-            return state[0]
-        else:
-            raise Exception("unknown opcode %d" % opcode)
+        opcode, modes = read_instruction(state[ip])
+        operation, output_handler = operations[opcode]
+        param_count = count_params(operation)
+        params = [param(n, modes) for n in range(1, param_count + 1)]
+        try:
+            result = operation(*params)
+        except ExitException:
+            return
+        ip += param_count + 1 # including opcode
+        ip = output_handler(result, state, ip)
 
 
 
 if __name__ == "__main__":
     input_loc = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'input')
-    part1(open(input_loc).readlines())
+    both(open(input_loc).readlines())
--- 2019/05/both.test.py
+++ 2019/05/both.test.py
@@ -1,11 +1,36 @@
-import unittest
+import unittest, io, sys, os
 
-from part1 import read_instruction
+from both import read_instruction, both
 
 class PartOneTest(unittest.TestCase):
 
     def test_read_instruction(self):
         self.assertEqual((2, [0, 1]), read_instruction(1002))
+
+    def assert_last_stdout_line(self, f, test_in, expected_out):
+        stdin, stdout = sys.stdin, sys.stdout
+        sys.stdin = io.StringIO(test_in)
+        sys.stdout = io.StringIO()
+        try:
+            f()
+            real_out = sys.stdout.getvalue().split('\n')[-2]
+        finally:
+            sys.stdin, sys.stdout = stdin, stdout
+        self.assertEqual(expected_out, real_out)
+
+
+    def test_part1(self):
+        f = lambda: both(self.initial_state())
+        self.assert_last_stdout_line(f, "1\n", str(13787043))
+
+    def test_part2(self):
+        f = lambda: both(self.initial_state())
+        self.assert_last_stdout_line(f, "5\n", str(3892695))
+
+    def initial_state(self):
+        input_loc = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'input')
+        with open(input_loc) as f:
+            return f.readlines()
 
 if __name__ == '__main__':
     unittest.main()
